6d5c07f3375f850d37eeb21a7b224e76ac0831a5
davidborja732 <dborjam@iesch.org>
2024-10-07 18:51:02 +0200

    07/10/2024

--- Programacion/Tema 1/Codigo/Primeros Ejercicios/src/Ejercicio_14.java
+++ Programacion/Tema 1/Codigo/Primeros Ejercicios/src/Ejercicio_14.java
@@ -1,9 +1,10 @@
 public class Ejercicio_14{
     public static void main(String [] args){
-        int segundos=9000;
+        int segundos=3665;
         int horas=segundos/3600;
-        int minutos=segundos/60;
-        System.out.println(horas+" "+minutos);
+        int minutos=(segundos%3600)/60;
+        int segundo=(segundos%3600)%60;
+        System.out.println(horas+" "+minutos+" "+segundo);
 
     }
 }
