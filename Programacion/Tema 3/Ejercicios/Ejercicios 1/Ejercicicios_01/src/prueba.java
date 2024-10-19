89d43c8765842ff46244a338e06b28a2d18da407
David Borja Mateo <dborjam@iesch.org>
2024-10-19 23:10:53 +0200

    19/10/2024

---
+++ Programacion/Tema 3/Ejercicios/Ejercicios 1/Ejercicicios_01/src/prueba.java
@@ -0,0 +1,8 @@
+public class prueba {
+    public static void main(String[] args) {
+        System.out.println();
+        String prueba="";
+        int numero=0;
+        float numero3= 1.67F;
+    }
+}
